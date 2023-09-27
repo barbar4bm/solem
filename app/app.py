7cb06a08f7ed9ceaaf05406ae3e500ea59b34fbc
ImLuu <45904735+ImLuu@users.noreply.github.com>
2023-09-27 15:33:41 -0300

    importaciones e inicio de codigo
    No hay mas implementacion porque el video cambia a jupyter

--- app/app.py
+++ app/app.py
@@ -1,5 +1,8 @@
 from flask import Flask, jsonify
 from carnet import carnet 
+import cv2 
+from PIL import Image
+import pytesseract
 
 app = Flask(__name__)
 
@@ -15,3 +18,6 @@ def index():
 
 if __name__ == '__main__': 
     app.run(debug=True,port=5000)
+
+im_file = "imagen" #nombre de la imagen, debe de ser una cedula
+im = Image.open(im_file)
