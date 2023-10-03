884677196d2ac9b56d229d7ba20f925479081fef
ImLuu <45904735+ImLuu@users.noreply.github.com>
2023-10-03 14:39:54 -0300

    algo

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from carnet import carnet 
-
+import cv2
+import sys 
 
 app = Flask(__name__)
 
@@ -12,6 +13,24 @@ def getCarnetCheck():
 @app.route('/')
 def index():
     return "<h1>CHECKID -- Solem<h1>"
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("Por favor, proporciona la ruta de la imagen como argumento.")
+        return
+    
+    img = cv2.imread(sys.argv[1])
+    
+    if img is None:
+        print("No se pudo cargar la imagen.")
+        return
+    
+    cv2.namedWindow("Example1", cv2.WINDOW_AUTOSIZE)
+    cv2.imshow("Example1", img)
+    cv2.waitKey(0)
+    cv2.destroyWindow("Example1")
+
 
 if __name__ == '__main__': 
     app.run(debug=True,port=5000)
