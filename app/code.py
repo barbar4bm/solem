85c13d5546359f78c18815ad8b19d6f69dbb2081
ImLuu <45904735+ImLuu@users.noreply.github.com>
2023-10-04 17:19:38 -0300

    nuevas pruebas

--- app/code.py
+++ app/code.py
@@ -4,11 +4,13 @@ import cv2
 import sys
 from PIL import Image
 from pylab import * 
+import numpy as np
+import pytesseract
 
 #cedula derecha
 
 image = array(Image.open('app/cedula.jpg'))
-
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 title('Cedula de Indentidad')
 
 #escala gray
@@ -22,13 +24,17 @@ gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
 clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
 rut_eq = clahe.apply(gray)
 
-plt.imshow(rut_eq,cmap='gray')
-
  # Binarización
 ret,rut_bin = cv2.threshold(rut_eq,127,255,cv2.THRESH_BINARY)
 
 # Binarización otsu
 ret2,rut_otsu = cv2.threshold(rut_eq, 127,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)   
-show()
 
+cv2.rectangle(rut_otsu, (30, 90), (175, 275), (0,0,255), 4) #foto grande
+cv2.rectangle(rut_otsu , (130, 15), (180, 45), (0,0,255), 4) #bandera chile
+cv2.rectangle(rut_otsu, (305, 170), (430, 210), (0,0,255), 4) #documento
+
+
+plt.imshow(rut_otsu, cmap='gray')
+show()
 #aplicar OCR
