fcbd028abae0b16a5026555b5397ae9369433351
Barbara Bahamondez <barbara.b.m@outlook.cl>
2023-10-06 10:59:56 -0300

    se avanza con la arquitectura

--- app/app.py
+++ app/app.py
@@ -1,17 +1,12 @@
-from flask import Flask, jsonify
-from carnet import carnet 
+from flask import Flask
+from routes import identity_card
 
 app = Flask(__name__)
-
-
-
-@app.route('/carnet')
-def getCarnetCheck():
-    return jsonify(carnet)
 
 @app.route('/')
 def index():
     return "<h1>CHECKID -- Solem<h1>"
 
 if __name__ == '__main__': 
+    app.register_blueprint(identity_card.identity_card, url_prefix='/identity_card')
     app.run(debug=True,port=5000)
---
+++ app/services/Sift.py
@@ -0,0 +1,3 @@
+class Sift():
+    def __init__(self) -> None:
+        pass
