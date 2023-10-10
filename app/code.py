0172466e78f0dde9a0fe3b9f1cd2c57b9bfe0a44
ImLuu <45904735+ImLuu@users.noreply.github.com>
2023-10-10 13:35:05 -0300

    codigo OCR primera parte

--- app/code.py
+++ app/code.py
@@ -6,11 +6,16 @@ from PIL import Image
 from pylab import * 
 import numpy as np
 import pytesseract
+from google.cloud import vision
+from google.cloud.vision_v1 import types
+
+import os
 
 #cedula derecha
 
-image = array(Image.open('app/cedula.jpg'))
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+#image = array(Image.open('app/cedula.jpg'))
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
+image= cv2.imread('app/cedula.jpg')
 title('Cedula de Indentidad')
 
 #escala gray
@@ -36,5 +41,27 @@ cv2.rectangle(rut_otsu, (305, 170), (430, 210), (0,0,255), 4) #documento
 
 
 plt.imshow(rut_otsu, cmap='gray')
+
 show()
-#aplicar OCR
+
+
+#aplicar tesseract para obtener informacion 
+# Establece la variable de entorno con la ruta de tu archivo .json (mala practica)
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "app/ocrtry-401614-37f6a7cca750.json"
+
+# Inicializa el cliente de Vision
+client = vision.ImageAnnotatorClient()
+
+# Abre la imagen desde un archivo
+with open('app/cedula.jpg', 'rb') as image_file:
+    content = image_file.read()
+
+image = vision.Image(content=content)
+
+# Usa el OCR de Google Vision
+response = client.text_detection(image=image)
+texts = response.text_annotations
+
+# Imprime el texto detectado
+for text in texts:
+    print('\n"{}"'.format(text.description))
