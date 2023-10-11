dd118fad7cd87e1275ab6d3913fe422e7a25f113
ImLuu <45904735+ImLuu@users.noreply.github.com>
2023-10-11 12:26:14 -0300

    prueba de codigos

--- app/tesseract.py
+++ app/tesseract.py
@@ -2,9 +2,18 @@ from PIL import Image
 import pytesseract
 import numpy as np
 from pytesseract import Output
-import cv2
+import cv2      
 
 pytesseract.pytesseract.tesseract_cmd =r'c:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
-image= pytesseract.image_to_string(Image.open('app/cedula.jpg'))
+imagen_color = cv2.imread('app/cedula.jpg')
+#imagen_rgb = cv2.cvtColor(imagen_color, cv2.COLOR_BGR2RGB)
+#imagen_gris = cv2.cvtColor(imagen_rgb, cv2.COLOR_RGB2GRAY)
+#clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+#rut_eq = clahe.apply(imagen_gris)
+# Binarización
+#ret,rut_bin = cv2.threshold(rut_eq,127,255,cv2.THRESH_BINARY)
+#ret2,rut_otsu = cv2.threshold(rut_eq, 127,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
+image= pytesseract.image_to_string(imagen_color)
 print(image)
 
