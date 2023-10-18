36c865bfb7f6b43d90dfab68ffdf37a591cd897d
ImLuu <45904735+ImLuu@users.noreply.github.com>
2023-10-18 17:43:37 -0300

    finales

--- app/code.py
+++ app/code.py
@@ -35,10 +35,21 @@ def rut_comparacion(foto1, foto2):
     else:
         return False
 
-image= cv2.imread('app/image/a4.jpg')
+image= cv2.imread('app/image/a8.jpg')
+
+#edges = cv2.Canny(image, 100, 200)
+#img_float = np.float32(image)/255.0
+#img_log = cv2.log(img_float)
+#gamma = 0.2
+#c = 1
+#img_filtered = np.exp(c * img_log) ** gamma
+#img_filtered = np.uint8(img_filtered * 255)
+
+#sobelx = cv2.Sobel(image, cv2.CV_64F, 1, 0, ksize=5)
+#sobely = cv2.Sobel(image, cv2.CV_64F, 0, 1, ksize=5)
+#img = cv2.GaussianBlur(image, (3, 3), sigmaX=0, sigmaY=0)
 
 title('Cedula de Indentidad')
-
 #escala gray
 
 # Cambio de espacio de color de BGR a RGB
@@ -50,28 +61,44 @@ gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
 clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
 rut_eq = clahe.apply(gray)
 
+equ = cv2.equalizeHist(rut_eq)
+
  # Binarización
 ret,rut_bin = cv2.threshold(rut_eq,100,255,cv2.THRESH_BINARY)
 
 # Binarización otsu
-ret2,rut_otsu = cv2.threshold(rut_eq, 127,255,cv2.THRESH_BINARY + cv2.THRESH_OTSU)   
+ret2,rut_otsu = cv2.threshold(rut_eq,127,255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)   
+
 
 #recorte de imagenes a obtener lectura 
-nombre_texto = rut_bin[171:211 , 291:826] #nombre check
-apellido_texto = rut_bin[89:152 , 292:658] #apellido check
-rut_grande= rut_bin[460:496 , 98:273] #rut grande check
-nacionalidad= rut_bin[220:265, 291:425] #nacionalidad check
-sexo= rut_otsu[222:260, 470:540] #no detecta genero
-fecha_nacimiento = rut_bin[276:311 , 250:480]#fecha nacimiento check
+nombre_texto = rut_bin[171:211 , 291:810] #nombre check
+apellido_texto = rut_bin[85:152 , 292:520] #apellido check
+rut_grande = rut_bin[460:496 , 98:273] #rut grande check
+nacionalidad = rut_bin[220:265, 291:425] #nacionalidad check
+#sexo = rut_otsu[222:263, 450:600] #no detecta genero
+fecha_nacimiento = rut_bin[276:311 , 250:480] #fecha nacimiento check
 doc_texto = rut_bin[275:310, 487:663] #numero documento check
-fecha_emision= rut_bin[330:369 , 292:463] #fecha emision check
-fechaV_texto = rut_bin[335:367 , 477:667] #fecha vencimiento check
-#rut_chico=rut_bin[282:308,686:808] #rut en foto pequeña 
+fecha_emision = rut_bin[330:369 , 292:463] #fecha emision check
+fechaV_texto = rut_bin[335:367 , 477:667] #fecha vencimiento check 
+#rut_chico=rut_otsu[282:308,686:808] #rut en foto pequeña falta mejorar su visibilidad
 
-#aplicar que las imagenes se vean mejor 
+#lectura trasera
+nacio_en= rut_bin[211:247 , 182:600] #check pero con detalles
+profesion = rut_bin[242:274,182:403]
+mrz = rut_otsu[354:492, 42:800]
 
-plt.imshow(rut_eq,cmap='gray')
+gray2 = cv2.medianBlur(mrz, 5)
+dst2 = cv2.adaptiveThreshold(gray2, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+
+
+
+plt.imshow(apellido_texto,cmap='gray')
 show()
 
-print("numero de documento: " , OCR(rut_eq))
+
+print("numero de documento: " , OCR(apellido_texto))
+print("numero de documento: " , OCR(nacionalidad))
+print("numero de documento: " , OCR(fecha_nacimiento))
+
 #print("numero de documento: " , OCR(rut_chico))
+
