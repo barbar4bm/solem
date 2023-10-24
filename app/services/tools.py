7e6a9ec1e2d72b4ec12fd326f4dee98765dd8c45
NicolasJorquera <jjh005@alumnos.ucn.cl>
2023-10-23 21:35:56 -0300

    se agrega funcion, escalar imagen

--- app/services/tools.py
+++ app/services/tools.py
@@ -109,6 +109,7 @@ def imagen_a_matriz(imagen_data):
 
 
 
+
 def leerQR(image):
 
     # Inicializar el detector de códigos QR con OpenCV
@@ -167,3 +168,63 @@ def numpy_to_png(imagen_np):
     retval, buffer_png = cv2.imencode('.png', imagen_np)
     png_bytes = buffer_png.tobytes()
     return png_bytes
+
+def recorte(rut_bin, rut_bin2):
+    # 500x800
+    #anverso
+    nombres = rut_bin[171:211 , 291:806]
+    apellidos = rut_bin[88:152 , 292:520]
+    RUN = rut_bin[460:496 , 98:273]
+    nacionalidad = rut_bin[220:265, 291:425]
+    sexo = rut_bin[222:263, 450:600]
+    fecha_nacimiento = rut_bin[276:311 , 250:480]
+    numero_documento = rut_bin[275:310, 487:663]
+    fecha_emision = rut_bin[330:369 , 292:463]
+    fecha_vencimiento = rut_bin[335:367 , 477:667]
+
+    #reverso
+    ciudad = rut_bin2[211:247 , 182:600]
+    profesion = rut_bin2[242:274,182:403]
+    mrz = rut_bin2[354:492, 42:800]
+    nombre_mrz = rut_bin2[440:482, 418:800]
+    apellido_mrz = rut_bin2[440:478, 44:375]
+    rut_mrz = rut_bin2[395:440,495:747]
+    documento_mrz = rut_bin2[354:398,170:400]
+
+    return {
+        "nombres": nombres,
+        "apellidos": apellidos,
+        "RUN": RUN,
+        "nacionalidad": nacionalidad,
+        "sexo": sexo,
+        "fecha_nacimiento": fecha_nacimiento,
+        "numero_documento": numero_documento,
+        "fecha_emision": fecha_emision,
+        "fecha_vencimiento": fecha_vencimiento,
+        "ciudad": ciudad,
+        "profesion": profesion,
+        "mrz": mrz,
+        "nombre_mrz": nombre_mrz,
+        "apellido_mrz": apellido_mrz,
+        "rut_mrz": rut_mrz,
+        "documento_mrz": documento_mrz
+    }
+
+
+def escalar_imagen(img, altura, anchura):
+    """
+    Escala una imagen a un tamaño especificado.
+    
+    Parámetros:
+    - img (numpy.ndarray): Imagen a escalar.
+    - altura (int): Altura deseada de la imagen escalada.
+    - anchura (int): Anchura deseada de la imagen escalada.
+    
+    Retorna:
+    - numpy.ndarray: Imagen escalada.
+    """
+    
+    if not isinstance(img, np.ndarray):
+        raise ValueError("El parámetro 'img' debe ser un objeto numpy.ndarray.")
+    
+    return cv2.resize(img, (anchura, altura))
